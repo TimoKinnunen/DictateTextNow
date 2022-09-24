0cadb92e74970f585148ad9c8f124a2cf5e55a4c
Timo Kinnunen <timo.kinnunen@bredband.net>
2022-09-24 19:49:26 +0200

    Add project files.

---
+++ DictateTextNow/Models/NavigationMenuItem.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace DictateTextNow.Models
+{
+    public sealed class NavigationMenuItem
+    {
+        public Symbol MenuSymbolIcon { get; set; }
+        public string MenuToolTip { get; set; }
+        public string MenuItemName { get; set; }
+    }
+}
+
+
---
+++ DictateTextNow/Views/InstructionsPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+
+namespace DictateTextNow.Views
+{
+    public sealed partial class InstructionsPage : Page
+    {
+        private readonly MainPage mainPage;
+
+        public InstructionsPage()
+        {
+            InitializeComponent();
+
+            SizeChanged += Page_SizeChanged;
+            Loaded += Page_Loaded;
+
+            mainPage = MainPage.CurrentMainPage;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetPageContentStackPanelWidth();
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            SetPageContentStackPanelWidth();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            // code here
+            // code here
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            // code here
+            // code here
+        }
+
+        private void SetPageContentStackPanelWidth()
+        {
+            PageContentStackPanel.Width = ActualWidth -
+                PageContentScrollViewer.Margin.Left -
+                PageContentScrollViewer.Padding.Right;
+        }
+
+        #region MenuAppBarButton
+        private void HomeAppBarButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            mainPage.GoToHomePage();
+            mainPage.MenuNavigationListView.SelectedIndex = 0;
+        }
+        #endregion MenuAppBarButton
+
+        private async void PdfButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var uri = new Uri("ms-appx:///Instruction/DictateTextNowInstructions.pdf");
+            var file = await StorageFile.GetFileFromApplicationUriAsync(uri);
+            bool success = await Launcher.LaunchFileAsync(file);
+        }
+
+    }
+}
